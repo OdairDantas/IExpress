920dceabbf84fab67c2315df027534b9e1854855
Odair dantas <odair-dantas@hotmail.com>
2020-09-08 02:48:48 -0300

    adicionando identity

---
+++ src/IExpress.Pagamentos.Infrastructure/Data/Contexts/IdentityContext.cs
@@ -0,0 +1,35 @@
+﻿using IExpress.Core.Communication.Mediator;
+using IExpress.Pagamentos.Infrastructure.Security;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace IExpress.Pagamentos.Infrastructure.Data.Contexts
+{
+    public class IdentityContext : IdentityDbContext<Usuario>
+    {
+        public IdentityContext()
+        {
+
+        }
+        public IMediatorHandler _bus { get; set; }
+
+        public IdentityContext(DbContextOptions<IdentityContext> options, IMediatorHandler rebusHandler) : base(options)
+        {
+            _bus = rebusHandler ?? throw new ArgumentNullException(nameof(rebusHandler));
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=IExpress;Trusted_Connection=true;");
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
---
+++ src/IExpress.Pagamentos.Infrastructure/Security/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IExpress.Pagamentos.Infrastructure.Security
+{
+    public class Usuario : IdentityUser
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+    }
+}
